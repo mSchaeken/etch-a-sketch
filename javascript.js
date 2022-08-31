9f90e7bcc61298032ed8c30207be3c6acc23828d
MathijsSchaeken <mathijs.schaeken@outlook.com>
2022-08-31 20:28:21 +0200

    Create initial HTML layout
    Added initial HTML elements in order to start adding code that will
    allow for selecting and creating the grid for the sketch functionality.
    Sections added to make eventual stylising with CSS easier.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* DOM */
+
+//QuerySelectors and listener in order to dynamically update grid based on slider value
+const gridSlider = document.querySelector('.grid-slider')
+
+//Created element and for loop in order to fill on-screen grid with divs
+const divGrid = document.createElement('div')
+
+for ()
