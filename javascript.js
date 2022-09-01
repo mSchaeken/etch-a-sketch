d6d38640ac34e79988bc84532e708fe58285f9fa
MathijsSchaeken <mathijs.schaeken@outlook.com>
2022-09-01 08:36:19 +0200

    Alter slider.oninput to dynamically create divs
    Added functionality to the slider oninput function. Whenever the value
    of the slider changes the grid div container will now first be emptied
    and filled again with the correct amount of divs selected by the user.

--- javascript.js
+++ javascript.js
@@ -7,15 +7,34 @@ gridSlider.textContent = gridSlider.value;
 gridSliderValue.append(gridSlider.textContent);
 
 gridSlider.oninput = function() {
+
     gridSliderValue.textContent = this.value;
+
+    while (divGrid.hasChildNodes()) {
+        divGrid.removeChild(divGrid.firstChild)
+    }
+    
+    for (i = 0; i < gridSliderValue.textContent; i++) {
+    
+        const divToAdd = document.createElement('div');
+        divToAdd.className = 'sketch-div';
+        divToAdd.id = 'sketch-div-' + i;
+    
+        divGrid.append(divToAdd)
+    }
 }
 
 
 
 
 //Created element and for loop in order to fill on-screen grid with divs
-const divGrid = document.createElement('div');
+const divGrid = document.querySelector('.div-grid')
 
 for (i = 0; i < gridSliderValue.textContent; i++) {
+    
+    const divToAdd = document.createElement('div');
+    divToAdd.className = 'sketch-div';
+    divToAdd.id = 'sketch-div-' + i;
 
+    divGrid.append(divToAdd)
 }
