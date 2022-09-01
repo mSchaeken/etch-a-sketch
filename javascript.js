ff517dc6a047f3170e85a4f2675750b6cfcedb95
MathijsSchaeken <mathijs.schaeken@outlook.com>
2022-09-01 16:49:03 +0200

    Fix grid bug
    Refactored grid generator function in order to create the
    correct amount of divs.

--- javascript.js
+++ javascript.js
@@ -5,19 +5,20 @@ const gridSlider = document.querySelector('#grid-slider');
 const gridSliderValue = document.querySelector('#grid-slider-value');
 const divGrid = document.querySelector('.div-grid');
 
-
 gridSlider.textContent = gridSlider.value;
 gridSliderValue.append(gridSlider.textContent);
 
+//Function neccesary to fill grid with divs for sketching
 gridSlider.oninput = function() {
 
     gridSliderValue.textContent = this.value;
+    gridSliderSize = this.value;
 
     while (divGrid.hasChildNodes()) {
         divGrid.removeChild(divGrid.firstChild)
     }
     
-    for (i = 0; i < gridSliderValue.textContent; i++) {
+    for (i = 0; i < (gridSliderSize * gridSliderSize); i++) {
     
         const divToAdd = document.createElement('div');
         divToAdd.className = 'sketch-div';
