22ef0e3e9f898473b9594733081949c58dbffd98
MathijsSchaeken <mathijs.schaeken@outlook.com>
2022-09-05 16:43:46 +0200

    Refactor all core functionality
    Finished a major refactor of all existing javascript code.
    
    Some key features are adding eventlisteners to each sketch div upon
    creating said div.
    
    Splitting up functionalities like creating the grid, clearing the grid,
    and setting the slider text into separate functions.
    
    And moving certain variables into functions as well.

--- javascript.js
+++ javascript.js
@@ -3,56 +3,19 @@
 //QuerySelectors and listeners in order to dynamically update grid based on slider value
 const divGrid = document.querySelector('.div-grid');
 
-let sketchDivs = document.querySelectorAll('.sketch-div')
 let gridSliderText = document.querySelector('#grid-slider-text');
 let gridSlider = document.querySelector('#grid-slider');
-let gridSliderValue = document.getElementById('grid-slider').value;
 
-let mouseDown = true
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-gridSliderText.textContent = gridSliderValue;
-
-window.onload = createSketchDivs()
-window.onload = attachSketchListener()
-
-gridSlider.addEventListener('input', () => {
-     updateRangeSlider();
-});
-
+gridSlider.addEventListener('input', updateRangeSlider);
 gridSlider.addEventListener('change', () => {
     createSketchDivs();
 });
 
-// gridSlider.addEventListener('change', () => {
-//     sketchDivs = document.querySelectorAll('.sketch-div');
-    
-//     sketchDivs.forEach((div) => {
-//         div.addEventListener('mousedown', () => {
-//                 div.className = 'sketch-div-transformed'
-//         });
-//     });
-// });
-
-gridSlider.addEventListener('change', () => {
-    sketchDivs = document.querySelectorAll('.sketch-div');
-    
-    sketchDivs.forEach((div) => {
-        div.addEventListener('mouseover', () => {
-                if mouseDown
-        });
-    });
-});
-
-// sketchDivs.forEach((div) => {
-//     div.addEventListener('mousedown', () => {
-//         div.className = 'sketch-div-transformed'
-//     });
-// });
-
 /* FUNCTIONS */
-
 
 //Function to update grid slider textcontent
 function updateRangeSlider(sliderValue=gridSlider.value) {
@@ -63,30 +26,27 @@ function updateRangeSlider(sliderValue=gridSlider.value) {
 function createSketchDivs(divAmount=gridSlider.value) {
 
     let gridSliderSize = parseInt(divAmount);
-
     document.getElementById('div-grid').style.gridTemplateColumns = `repeat(${gridSliderSize}, 1fr)`;
     document.getElementById('div-grid').style.gridTemplateRows = `repeat(${gridSliderSize}, 1fr)`;
 
+    setSliderText()
     clearGrid()
     
     for (i = 0; i < (gridSliderSize * gridSliderSize); i++) {
     
         const divToAdd = document.createElement('div');
-        divToAdd.className = 'sketch-div';
-        divToAdd.addEventListener('mouseover', changeClass)
-        divToAdd.addEventListener('mousedown', changeClass)
 
+        divToAdd.className = 'sketch-div';
+        divToAdd.addEventListener('mouseover', changeColor);
+        divToAdd.addEventListener('mousedown', changeColor);
         divGrid.append(divToAdd);
     }
 }
 
-//Function to attach listeners to every created sketch div
-function attachSketchListener() {
-    sketchDivs.forEach((div) => {
-        div.addEventListener('mousedown', () => {
-            div.className = 'sketch-div-transformed'
-        });
-    });
+function changeColor(e) {
+    if (mouseDown === true) {
+        e.target.style.backgroundColor = '#FF1053'
+    }
 }
 
 function clearGrid() {
@@ -94,3 +54,9 @@ function clearGrid() {
         divGrid.removeChild(divGrid.firstChild);
     }
 }
+
+function setSliderText() {
+    gridSliderText.textContent = gridSlider.value
+}
+
+window.onload = createSketchDivs()
